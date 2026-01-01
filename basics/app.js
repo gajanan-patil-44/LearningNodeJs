836f97f75c0848682907256e055c50441a226dbb
Gajanan Patil <gajananpatil7491@gmail.com>
2026-01-01 20:03:07 +0530

    created first server in node

---
+++ basics/app.js
@@ -0,0 +1,11 @@
+const { log } = require('console');
+const HTTP = require('http');
+const SERVER = HTTP.createServer((req,res) => {
+    console.log(req);
+})
+
+const PORT = 3000;
+SERVER.listen(PORT,() => {
+    console.log(`server running on address http://localhost:${PORT}`);
+    
+})
